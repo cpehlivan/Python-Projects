7bec7b1af7ab0919526a810ecfac4385729bc5b1
cpehlivan <can.pehlivan1998@hotmail.de>
2022-03-02 17:21:43 +0100

    navites to chat and writes and sends it

--- whatsAppBot.py
+++ whatsAppBot.py
@@ -1,3 +1,4 @@
+from multiprocessing.connection import wait
 from turtle import pos, position
 import pyautogui as pt
 import pyperclip as pc
@@ -13,9 +14,13 @@ class WhatsApp:
         self.last_message = " "
         
         
+        
+    # findet die Position des Bildes und geht darauf zu
+    #
+    #    
     def nav_green_dot(self):
         try:
-            position = pt.locateAllOnScreen('NewMessageSign.png', confidence=.8)
+            position = pt.locateAllOnScreen('askitompb.png', confidence=.8)
             position = list(position)
             pt.moveTo(position[0].left, position[0].top, duration=self.speed)
             pt.moveRel(-100,0,duration=self.speed)
@@ -24,5 +29,31 @@ class WhatsApp:
             print("Exception(nav_green_dot): " ,e)
             
             
-wa_bot = WhatsApp(speed=.5, click_speed=.4)
+    def write_answer(self, text):
+        try:
+            position = pt.locateAllOnScreen('Bueroklammerzentriert.png', confidence=.8)
+            position = list(position)
+            pt.moveTo(position[0].left, position[0].top, duration=self.speed)
+            pt.moveRel(100,25,duration=self.speed)
+            pt.click(interval=self.click_speed)
+            pt.write(text)
+        except Exception as e:
+            print("Exception(bueroklammer): " ,e)
+            
+            
+    def send_answer(self):
+        try:
+            position = pt.locateAllOnScreen("sendpicture.png", confidence=.9)
+            position = list(position)
+            pt.moveTo(position[0].left, position[0].top, duration=self.speed) 
+            pt.moveRel(25,25)
+            pt.click(interval=self.click_speed)  
+        except Exception as e:
+            print("Exception(sending): " ,e)
+            
+            
+            
+wa_bot = WhatsApp(speed=1, click_speed=.4)
 wa_bot.nav_green_dot()
+wa_bot.write_answer("Seni Seviyorum Askitom!!!")
+wa_bot.send_answer()
